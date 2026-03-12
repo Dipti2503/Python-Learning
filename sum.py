b16d72590b661c16bfbae5a223b0a5bf73ad6a57
Dipti Ranjan <ranjandipti2016@gmail.com>
2026-03-13 01:15:10 +0530

    My first commit

---
+++ sum.py
@@ -0,0 +1,10 @@
+# Take input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate sum
+sum_result = num1 + num2
+
+# Display the result
+print("The sum is:", sum_result)
+
